ceabd68419abfd33005a856f359b1ff10a93cb69
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-05-18 08:40:22 +0900

    basics/Section2-6 jestの設定まで。

---
+++ basics/src/app/Utils.ts
@@ -0,0 +1,5 @@
+export class Utils {
+  static toUppercase(arg: string) {
+    return '';
+  }
+}
---
+++ basics/src/test/Utils.test.ts
@@ -0,0 +1,9 @@
+import { Utils } from '../app/Utils';
+
+describe('Utils test suite', () => {
+  test('first test', () => {
+    console.log('test works!!');
+
+    const abc = Utils.toUppercase('abc');
+  });
+});
