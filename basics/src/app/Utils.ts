fc2eb15aa0c90de161ebc11ed931f1ef27f1fa21
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-05-18 09:19:01 +0900

    basics/Section2-7 オブジェクトの比較は、toEqual。

--- basics/src/app/Utils.ts
+++ basics/src/app/Utils.ts
@@ -1,5 +1,15 @@
+import { UrlWithParsedQuery, parse } from 'url';
+
 export class Utils {
+  static parseUrl(url: string): UrlWithParsedQuery {
+    return parse(url, true);
+  }
+
   static toUppercase(arg: string) {
-    return '';
+    return arg.toUpperCase();
+  }
+
+  static getUsers() {
+    return ['user1', 'user2', 'user3'];
   }
 }
--- basics/src/test/Utils.test.ts
+++ basics/src/test/Utils.test.ts
@@ -2,8 +2,35 @@ import { Utils } from '../app/Utils';
 
 describe('Utils test suite', () => {
   test('first test', () => {
-    console.log('test works!!');
+    const result = Utils.toUppercase('abc');
+    expect(result).toBe('ABC');
+  });
 
-    const abc = Utils.toUppercase('abc');
+  test('parse simple URL', () => {
+    const parsedUrl = Utils.parseUrl('http://localhost:8080/login');
+
+    expect(parsedUrl.href).toBe('http://localhost:8080/login');
+    expect(parsedUrl.port).toBe('8080');
+    expect(parsedUrl.protocol).toBe('http:');
+  });
+
+  test('parse URL with query', () => {
+    const parsedUrl = Utils.parseUrl(
+      'http://localhost:8080/login?user=test&password=test123!'
+    );
+
+    const expected = {
+      user: 'test',
+      password: 'test123!',
+    };
+
+    // オブジェクトの比較は、toEqual
+    expect(parsedUrl.query).toEqual(expected);
+  });
+
+  test('array', () => {
+    const users = Utils.getUsers();
+
+    expect(users).toEqual(['user1', 'user2', 'user3']);
   });
 });
